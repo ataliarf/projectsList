525c1c7e4244c3fc8071fb7064fcbf38b679c867
AtaliaRefua <102587122+AtaliaRefua@users.noreply.github.com>
2022-08-05 12:28:03 +0300

    friday

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CrudService } from '../shared/crud.service';
+import { UserEmailService } from '../shared/data.service';
 import { Project } from './../shared/project';
 import { ToastrService } from 'ngx-toastr';
 
@@ -12,26 +13,40 @@ import { ToastrService } from 'ngx-toastr';
 export class ProjectListComponent implements OnInit {
   p: number = 1;
   Project: Project[];
+  ProjectsForCurrentUser: Project[];
   hideWhenNoProject: boolean = false;
   noData: boolean = false;
   preLoader: boolean = true;
   counter: number = 1;
   emailOfSomeoneToInvite: string;
+  userEmail: string | undefined;
 
-  constructor(public crudApi: CrudService, public toastr: ToastrService) {}
+
+  // constructor(public crudApi: CrudService, public toastr: ToastrService,  private data: UserEmailService) {}
+  constructor(public crudApi: CrudService, public toastr: ToastrService, private dataFromService: UserEmailService) {}
 
   ngOnInit() {
+    this.userEmail = this.dataFromService.getUserEmail();
     this.dataState();
     let s = this.crudApi.GetProjectsList();
     s.snapshotChanges().subscribe((data) => {
       this.Project = [];
+      this.ProjectsForCurrentUser= [];
       data.forEach((item) => {
         let a = item.payload.toJSON();
         a['$key'] = item.key;
         this.Project.push(a as Project);
+       if( (a['permissions'][0] && a['permissions'][0]   == this.userEmail) || (a['permissions'][1] && a['permissions'][1]  == this.userEmail)  ||  a['permissions'][2]  && (a['permissions'][2] == this.userEmail) ||
+        ( a['permissions'][3] && a['permissions'][3] == this.userEmail) || (a['permissions'][4] && a['permissions'][4]  == this.userEmail) || (a['permissions'][5] && a['permissions'][5] == this.userEmail) ||
+        (a['permissions'][6] && a['permissions'][6] == this.userEmail) || (a['permissions'][7] && a['permissions'][7] == this.userEmail) ||  (a['permissions'][8] && a['permissions'][8] == this.userEmail) ) { 
+              this.ProjectsForCurrentUser.push(a as Project); 
+        }        
+
       });
     });
   }
+
+
 
   dataState() {
     this.crudApi
@@ -48,6 +63,7 @@ export class ProjectListComponent implements OnInit {
         }
       });
   }
+
 
   deleteProject(project: Project) {
     if (window.confirm('Are sure you want to delete this project ?')) {
